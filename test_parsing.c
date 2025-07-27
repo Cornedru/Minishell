71268d5bedb84ef07f0b861bd8516af27156d157
Nail Dehmej <ndehmej@k2r1p10.42mulhouse.fr>
2025-07-27 03:26:24 +0200

    plus de leak sur exit ajout sur le fichier .supp1111

--- test_parsing.c
+++ test_parsing.c
@@ -41,6 +41,63 @@ static int	process_line(char *line, t_sys *sys)
 	return (status);
 }
 
+// static int	shell_loop(t_sys *sys)
+// {
+// 	char	*line;
+// 	int		stdin_copy;
+
+// 	stdin_copy = dup(STDIN_FILENO);
+// 	while (!sys->exit)
+// 	{
+// 		if (g_signal == SIG_INTERRUPT)
+// 		{
+// 			sys->exit_status = 130;
+// 			g_signal = SIG_NONE;
+// 		}
+// 		line = readline(PROMPT);
+// 		if (!line)
+// 		{
+// 			ft_putstr_fd("exit\n", 1);
+// 			break ;
+// 		}
+// 		if (*line)
+// 			add_history(line);
+// 		process_line(line, sys);
+// 		gc_free(line);
+// 		dup2(stdin_copy, STDIN_FILENO);
+// 	}
+// 	close(stdin_copy);
+// 	return (sys->exit_status);
+// }
+
+void gc_clear_all(t_gc *gc)
+{
+    t_gc_node *node;
+    t_gc_node *tmp;
+
+	node = NULL;
+	if (!node)
+    	return;
+    node = gc->permanent;
+    while (node)
+    {
+        free(node->ptr);
+        tmp = node;
+        node = node->next;
+        free(tmp);
+    }
+    gc->permanent = NULL;
+    node = gc->temporary;
+    while (node)
+    {
+        free(node->ptr);
+        tmp = node;
+        node = node->next;
+        free(tmp);
+    }
+    gc->temporary = NULL;
+}
+
 static int	shell_loop(t_sys *sys)
 {
 	char	*line;
@@ -65,10 +122,14 @@ static int	shell_loop(t_sys *sys)
 		process_line(line, sys);
 		gc_free(line);
 		dup2(stdin_copy, STDIN_FILENO);
+
+		// ✅ Libère tout ce qui a été malloc dans cette boucle
+		gc_clear_all(sys->gc);
 	}
 	close(stdin_copy);
 	return (sys->exit_status);
 }
+
 
 // int	main(int argc, char **argv, char **envp)
 // {
@@ -86,36 +147,8 @@ static int	shell_loop(t_sys *sys)
 // 	return (exit_status);
 // }
 
-void gc_clear_all(t_gc *gc)
-{
-    t_gc_node *node;
-    t_gc_node *tmp;
 
-	node = NULL;
-	if (!node)
-		return;
-    // Libérer la liste permanente
-    node = gc->permanent;
-    while (node)
-    {
-        free(node->ptr);
-        tmp = node;
-        node = node->next;
-        free(tmp);
-    }
-    gc->permanent = NULL;
 
-    // Libérer la liste temporaire
-    node = gc->temporary;
-    while (node)
-    {
-        free(node->ptr);
-        tmp = node;
-        node = node->next;
-        free(tmp);
-    }
-    gc->temporary = NULL;
-}
 
 // int	main(int argc, char **argv, char **envp)
 // {
@@ -156,42 +189,6 @@ int main(int argc, char **argv, char **envp)
     rl_clear_history();
     return (exit_status);
 }
-// Exemple pour libérer sys->env_lst si c'est une liste chaînée
-// void free_env_lst(t_env *env_lst)
-// {
-//     t_env *tmp;
-//     while (env_lst)
-//     {
-//         tmp = env_lst->next;
-//         free(env_lst->name);     
-//         free(env_lst->content);  
-//         free(env_lst);
-//         env_lst = tmp;
-//     }
-// }
 
-
-
-// int main(int argc, char **argv, char **envp)
-// {
-//     t_sys *sys;
-//     int exit_status;
-
-//     (void)argc;
-//     (void)argv;
-//     setup_signals();
-//     if (init_shell(&sys, envp))
-//         return (1);
-//     exit_status = shell_loop(sys);
-
-//     rl_clear_history();
-
-//     gc_clear_all(sys->gc);
-//     gc_destroy();
-
-//     free_env_lst(sys->env_lst);  
-//     free(sys->gc);              
-//     free(sys);
-
-//     return (exit_status);
-// }
+you must also verify the abscence of memory leak. any memory allocated on the heap must be properly freed before the end of execution.
+you are allowed to use any of the different tools available ont the cumputer, such as leaks, valgrind, or e_fence.
